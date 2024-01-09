e7ddcb8e18ac225786beb8839e5f2fe53d5d108c
André <AndreR@ppedv.de>
2024-01-09 09:50:24 +0100

    EfCore für 🍕🍕🍕

---
+++ ppedv.PizzaPizzaPizza/ppedv.PizzaPizzaPizza.Data.EfCore/PizzaContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.PizzaPizzaPizza.Model;
+
+namespace ppedv.PizzaPizzaPizza.Data.EfCore
+{
+    public class PizzaContext : DbContext
+    {
+        string conString;
+
+        public PizzaContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString).UseLazyLoadingProxies();
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Belag> Belaege { get; set; }
+    }
+}
